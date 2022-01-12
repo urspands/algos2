b93f66a89c61946956fbdd2902244cb429d3118a
Pandiarajan Subramani <raj@sproutling.com>
2022-01-12 04:30:12 -0800

    added more sort problems.

--- app/src/main/java/com/raj/algorithms/MainActivity.kt
+++ app/src/main/java/com/raj/algorithms/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.raj.algorithms
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import com.raj.algorithms.databinding.ActivityMainBinding
 import com.raj.algorithms.sorting.Sortings
 
@@ -33,11 +34,25 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        Sortings.selectionSort(arrayOf(10,5,4,6,2,7,1))
-        Sortings.bubbleSort(arrayOf(10,5,4,6,2,7,1))
-        Sortings.insertionSort(arrayOf(10,5,4,6,2,7,1))
-        val array =arrayOf(10,5,4,6,2,7,1)
-        Sortings.mergeSort(array,0,array.size-1)
+//        Sortings.selectionSort(arrayOf(10,5,4,6,2,7,1))
+//        Sortings.bubbleSort(arrayOf(10,5,4,6,2,7,1))
+//        Sortings.insertionSort(arrayOf(10,5,4,6,2,7,1))
+//        val array = arrayOf(7, 5, 8, 3, 9, 4, 1, 7)
+//        Log.d(
+//            "MergeSort",
+//            "onCreate: ${Sortings.mergeSortPr(array, 0, array.size - 1).contentToString()}"
+//        )
+//        Sortings.findIf2NumSumExists(arrayOf(10,5,4,6,2,7,1),21)
+//        Sortings.findIf2NumSumExistsUsingHashSet(arrayOf(10,5,4,6,2,7,1),8)
+//        Sortings.findIf2NumSumExistsReturnIndices(arrayOf(10,5,4,6,2,7,1),10)
+//        Sortings.canAttendMeeting(arrayOf(arrayOf(3, 6), arrayOf(1, 2), arrayOf(3, 4)))
+//        Sortings.find3numbsSumsTo0(arrayOf(-1,1,0,2,-1,4))
+//        Sortings.intersectionOf3SortedArrays(arrayOf(-1,2,3,4,5),arrayOf(-1,2,5,7,9),arrayOf(-1,3,4,5,8))
+//        Sortings.groupTheNumbers(arrayOf(8,4,9,5,2,9,5,7,10))
+//        Sortings.mergerFirstIntoSecond(arrayOf(1, 3, 5), arrayOf(2, 4, 6, 0, 0, 0))
+//        Sortings.orderRGB(arrayOf('R','B','R','G','B','R','R','G'))
+        Sortings.orderRGB(arrayOf('G', 'B', 'G', 'G', 'R', 'B', 'R', 'G'))
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/raj/algorithms/sorting/Sortings.kt
+++ app/src/main/java/com/raj/algorithms/sorting/Sortings.kt
@@ -94,4 +94,279 @@ object Sortings {
         }
         return tempArray
     }
+
+
+    // Sorting Problems
+
+//    Find if sum of 2 numbers matches the target
+
+    fun findIf2NumSumExists(array: Array<Int>, target: Int): Boolean {
+        array.sort()
+        var start = 0
+        var end = array.size - 1
+        while (start < end) {
+            val total = array[start] + array[end]
+            when {
+                total == target -> {
+                    Log.d(TAG, "findIf2NumSumExists: Exists: true")
+                    return true
+                }
+                total > target -> {
+                    end--
+                }
+                else -> start++
+            }
+        }
+        Log.d(TAG, "findIf2NumSumExists: Exists: false")
+        return false
+    }
+
+    fun findIf2NumSumExistsUsingHashSet(array: Array<Int>, target: Int): Boolean {
+        val hashSet = HashSet<Int>()
+        hashSet.addAll(array)
+        for (num in array) {
+            if (hashSet.contains(target - num)) {
+                Log.d(TAG, "findIf2NumSumExistsUsingHashSet: Exists: true")
+                return true
+            }
+        }
+
+        Log.d(TAG, "findIf2NumSumExistsUsingHash: Exists: false")
+        return false
+    }
+
+    fun findIf2NumSumExistsReturnIndices(array: Array<Int>, target: Int): IntArray {
+        val hashMap = HashMap<Int, Int>()
+        val retVal = IntArray(2)
+        for (i in array.indices) {
+            hashMap[i] = array[i]
+        }
+        for (i in array.indices) {
+            if (hashMap.containsValue(target - array[i])) {
+                retVal[1] = i
+                retVal[0] = hashMap.filter { it.value == target - array[i] }.keys.first()
+            }
+        }
+        Log.d(TAG, "findIf2NumSumExistsReturnIndices: ${retVal.contentToString()}")
+        return retVal
+    }
+
+    fun canAttendMeeting(array: Array<Array<Int>>): Boolean {
+        array.sortBy { it[0] }
+        Log.d(TAG, "canAttendMeeting: ${array.contentDeepToString()}")
+        for (i in 0..array.size - 2) {
+            if (array[i][1] > array[i + 1][0]) {
+                Log.d(TAG, "canAttendMeeting: false")
+                return false
+            }
+        }
+        Log.d(TAG, "canAttendMeeting: true")
+        return true
+    }
+
+    //a+b+c=0
+    fun find3numbsSumsTo0(array: Array<Int>) {
+        array.sort()
+        val retVal = HashSet<ArrayList<Int>>()
+        for (i in array.indices) {
+            val target = -array[i]
+            var start = i + 1
+            var end = array.size - 1
+            while (start < end) {
+                when {
+                    array[start] + array[end] == target -> {
+                        retVal.add(arrayListOf(array[i], array[start], array[end]))
+                        start++
+                        end--
+                    }
+                    array[start] + array[end] > target -> {
+                        end--
+                    }
+                    else -> {
+                        start++
+                    }
+                }
+            }
+        }
+        Log.d(TAG, "find3numbsSumsTo0:${retVal.toArray().contentDeepToString()}")
+    }
+
+    fun intersectionOf3SortedArrays(
+        array1: Array<Int>,
+        array2: Array<Int>,
+        array3: Array<Int>
+    ): Array<Int> {
+        var retval: Array<Int> = arrayOf()
+        var i = 0
+        var j = 0
+        var k = 0
+        while (i < array1.size && j < array2.size && k < array3.size) {
+            if (array1[i] == array2[j] && array1[i] == array3[k]) {
+                retval = retval.plus(array1[i])
+                i++
+                j++
+                k++
+            } else {
+                val min1 = array1[i].coerceAtMost(array2[j])
+                val min2 = min1.coerceAtMost(array3[k])
+                if (min2 == array1[i]) {
+                    i++
+                }
+                if (min2 == array2[j]) {
+                    j++
+                } else {
+                    k++
+                }
+            }
+        }
+        Log.d(TAG, "intersectionOf3SortedArrays: ${retval.contentToString()}")
+        return retval
+    }
+
+    /**   You are given an integer array arr of size n. Group and rearrange them (in-place) into
+     * evens and odds in such a way that group of all even integers appears on the left side and
+     * group of all odd integers appears on the right side.
+    Example
+    Input: [1, 2, 3, 4]
+    Output: [4, 2, 1, 3]
+    Order does not matter. Other valid solutions are:
+    [2, 4, 1, 3]
+    [2, 4, 3, 1]
+    [4, 2, 3, 1]*/
+    fun groupTheNumbers(array: Array<Int>) {
+        var i = 0
+        var j = array.size - 1
+        while (i < j) {
+            while (array[i] % 2 == 0 && i < array.size - 1) {
+                i++
+            }
+            while (array[j] % 2 != 0 && j > 0) {
+                j--
+            }
+            if (i < j) {
+                val temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+                i++
+                j--
+            }
+        }
+        Log.d(TAG, "groupTheNumbers: ${array.contentToString()}")
+    }
+
+    fun mergeSortPr(array: Array<Int>, start: Int, end: Int): Array<Int> {
+        if (start == end) {
+            return array
+        }
+        val mid = (start + end) / 2
+        mergeSortPr(array, start, mid)
+        mergeSortPr(array, mid + 1, end)
+        var i = start
+        var j = mid + 1
+        var tempArray = intArrayOf()
+        while (i <= mid && j <= end) {
+            if (array[i] < array[j]) {
+                tempArray = tempArray.plus(array[i])
+                i++
+            } else {
+                tempArray = tempArray.plus(array[j])
+                j++
+            }
+        }
+        while (i <= mid) {
+            tempArray = tempArray.plus(array[i])
+            i++
+        }
+        while (j <= end) {
+            tempArray = tempArray.plus(array[j])
+            j++
+        }
+        var m = start
+        for (arr in tempArray) {
+            array[m] = arr
+            m++
+        }
+        return array
+    }
+
+    fun mergerFirstIntoSecond(array1: Array<Int>, array2: Array<Int>): Array<Int> {
+        var arr2EndPtr = array2.size - 1
+//        var arr2StartPtr = 0
+        var arr1EndPtr = array1.size - 1
+        var arr2ValEndPtr = (array2.size / 2) - 1
+
+        while (arr1EndPtr >= 0 && arr2ValEndPtr >= 0) {
+            if (array1[arr1EndPtr] > array2[arr2ValEndPtr]) {
+                array2[arr2EndPtr] = array1[arr1EndPtr]
+                arr2EndPtr--
+                arr1EndPtr--
+            } else {
+                array2[arr2EndPtr] = array2[arr2ValEndPtr]
+                arr2EndPtr--
+                arr2ValEndPtr--
+            }
+        }
+
+        while (arr1EndPtr >= 0) {
+            array2[arr2EndPtr] = array1[arr1EndPtr]
+            arr2EndPtr--
+            arr1EndPtr--
+        }
+        Log.d(TAG, "mergerFirstIntoSecond: ${array2.contentToString()}")
+        return array2
+    }
+
+    fun orderRGB(arr:Array<Char>){
+        var ptr =0
+        var endPtr = arr.size-1
+//        while(arr[ptr]=='R'){
+//            ptr++
+//        }
+
+        Log.d(TAG,"ptr ::$ptr");
+        while(ptr<=endPtr){
+            Log.d(TAG,"R loop::"+ arr.contentToString());
+            Log.d(TAG, "R loop::ptr ::$ptr");
+            Log.d(TAG, "R loop::endPtr ::$endPtr");
+            while(arr[ptr]=='R'){
+                ptr++;
+            }
+            if(arr[endPtr] =='R'){
+
+                Log.d(TAG, "Before swapping::ptr ::$ptr || endPtr :: $endPtr");
+                Log.d(TAG,"SWAPPING::"+arr[ptr] +" && "+arr[endPtr])
+                val tmp = arr[ptr]
+                arr[ptr]=arr[endPtr]
+                arr[endPtr]=tmp
+                ptr++
+            }
+            endPtr--
+        }
+        endPtr = arr.size-1;
+//        while(arr[ptr]=='G'){
+//            ptr++;
+//        }
+        Log.d(TAG,"ptr ::$ptr");
+        while(ptr<=endPtr){
+            Log.d(TAG,"G loop::"+arr.contentToString())
+            Log.d(TAG,"G loop::ptr ::$ptr")
+            Log.d(TAG,"G loop::endPtr ::$endPtr")
+            while(arr[ptr]=='G'){
+                ptr++
+            }
+            if(arr[endPtr] =='G'){
+
+                Log.d(TAG, "Before swapping::ptr ::$ptr || endPtr :: $endPtr");
+                Log.d(TAG,"SWAPPING::"+arr[ptr] +" && "+arr[endPtr])
+                val tmp = arr[ptr]
+                arr[ptr]=arr[endPtr]
+                arr[endPtr]=tmp
+                ptr++
+            }
+            endPtr--
+        }
+
+        Log.d(TAG,arr.contentDeepToString());
+    }
+
 }
